c718d3a609de2f242d9558c88180d09cc4d8caa4
JanhviAgrawal <workjanhvi21@gmail.com>
2026-01-03 19:25:14 +0530

    NodeJS

--- Stories/config/db.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const URI = "mongodb://localhost:27017/Story-Managment";
-
-mongoose.connect(URI)
-    .then(() => {
-        console.log("MongoDB is Connected..");
-    }).catch((err) => {
-        console.log("MongoDB is not Connected..", err);
-    });
